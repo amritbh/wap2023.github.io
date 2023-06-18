843d38dc14a876334fa91ae3ea265f26f3a2f64d
amritbh <bhattarai.amrit@outlook.com>
2023-06-18 14:37:27 -0500

    serverside code

---
+++ ShoppingCart Project/shoppingcart-server/model/users.js
@@ -0,0 +1,16 @@
+const users=[
+    {
+        username:"Pawan",
+        password:"pawan123"
+    },
+    {
+        username:"Amrit",
+        password:"amrit123"
+    },
+    {
+        username:"Tina",
+        password:"tina123"
+    }
+];
+
+module.exports=users;
